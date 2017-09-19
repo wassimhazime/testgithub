614512e6438d36b6bf23aaa0846f3f8e86c1c8d2
wassimhazime <wassim.hazime@gmail.com>
2017-09-19 11:14:39 +0200

    style scc

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link href="style.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
         <?php
